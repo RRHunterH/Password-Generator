78e0b93669e7a56c457b2c31f95a5668f2f62d83
jimhunterangie Hurst  .̲͚͍̬̩͇͙̬̮̹͎͖̘̹͂̓̒̈́̾̈́̾͑́͋͋͋̓ͣ̀ <hangingwithhunter4@gmail.com>
2023-12-10 02:54:51 -0500

    added the 1st prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,23 @@
+var generateBtn = document.querySelector("#generate");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numericChars = "0123456789";
   var specialChars = "!@#$%^&*()_-+=<>?/{}[]";
+  var length = prompt("Enter the length of the password (between 8 and 128 characters):");
+  length = parseInt(length);
 
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a valid password length between 8 and 128 characters.");
+    return "";
+  }
 }
