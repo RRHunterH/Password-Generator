b07230d6ceb39cdd95c11f647595aabf9e862ed7
jimhunterangie Hurst  .̲͚͍̬̩͇͙̬̮̹͎͖̘̹͂̓̒̈́̾̈́̾͑́͋͋͋̓ͣ̀ <hangingwithhunter4@gmail.com>
2023-12-10 02:48:24 -0500

    adding base var's

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,8 @@
-// Assignment code here
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+function generatePassword() {
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_-+=<>?/{}[]";
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
